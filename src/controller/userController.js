7a930ef658f40925a3b9a165112fec4d70e06235
carlmnm <carlossmiguell@gmail.com>
2023-03-03 03:27:58 -0300

    feat: POST /signin

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,8 @@
 import { db } from "../config/database.conection.js";
-import { v4 as uuid } from "uuid"
+import { v4 as uuidV4 } from "uuid"
 
 export async function postSignup(req, res) {
-    const {name, email, password, confirmPassword} = req.body
+    const { name, email, password, confirmPassword } = req.body
     const userExists = await db.query(`SELECT * FROM users WHERE email = $1;`, [email])
     if (userExists.rows[0]) {
         return res.status(409).send("você já existe")
@@ -10,7 +10,7 @@ export async function postSignup(req, res) {
     if (password !== confirmPassword) {
         return res.sendStatus(422)
     }
-    try{
+    try {
         await db.query(`INSERT INTO users (nome, email, senha)
         VALUES ($1, $2, $3);`, [name, email, password])
         res.sendStatus(201)
@@ -20,8 +20,8 @@ export async function postSignup(req, res) {
     }
 }
 
-export async function postSignin (req, res) {
-    const {email, password}  = req.body
+export async function postSignin(req, res) {
+    const { email, password } = req.body
     const user = await db.query(`SELECT * FROM users WHERE email = $1;`, [email])
 
     if (user.rowCount === 0 || user.rows[0].senha !== password) {
@@ -29,11 +29,10 @@ export async function postSignin (req, res) {
     }
 
     try {
-        const myToken = uuid()
-        const objectToken = {token: myToken}
+        const token = uuidV4()
         await db.query(`INSERT INTO tokens (user_id, token)
         VALUES ($1, $2);`, [user.rows[0].id, token])
-        res.send(objectToken).status(200)
+        return res.status(200).send({ token: token })
     } catch (error) {
         res.send(error)
     }
