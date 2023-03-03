58d91c5391e6ae079bf905d65543450225ce9410
carlmnm <carlossmiguell@gmail.com>
2023-03-03 17:32:52 -0300

    feat: GET /users/me

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -32,9 +32,47 @@ export async function postSignin(req, res) {
         const token = uuidV4()
         await db.query(`INSERT INTO tokens ("userId", token)
         VALUES ($1, $2);`, [user.rows[0].id, token])
-        
+
         return res.status(200).send({ token })
     } catch (error) {
         res.send(error)
     }
+}
+
+export async function showUserData(req, res) {
+    const { authorization } = req.headers
+    const token = authorization?.replace('Bearer ', '')
+
+    try {
+        const userIsLoged = await db.query(`SELECT * FROM tokens WHERE token = $1;`, [token])
+        const user = await db.query(`SELECT * FROM users WHERE id = $1;`, [userIsLoged.rows[0].userId])
+
+        if (!token || userIsLoged.rowCount === 0) return res.sendStatus(401)
+
+        const totalViews = await db.query(`SELECT SUM (views) AS total FROM shortys WHERE user_id = $1;`, [userIsLoged.rows[0].userId])
+
+        const myShortLinks = await db.query(`SELECT shortys.id, shortys.shorted_url, shortys.views FROM shortys WHERE user_id = $1;`, [userIsLoged.rows[0].userId])
+
+        const myUrls = await db.query(`SELECT urls.url FROM urls WHERE user_id = $1;`, [userIsLoged.rows[0].userId])
+
+        const shortedData = myShortLinks.rows.map((item) => {
+            return {
+                id: item.id,
+                shortUrl: item.shorted_url,
+                visitCount: item.views
+            }
+        })
+
+        const userData = {
+            id: userIsLoged.rows[0].userId,
+            name: user.rows[0].nome,
+            visitCount: totalViews.rows[0].total,
+            shortenedUrls: shortedData
+        }
+
+        res.status(200).send(userData)
+    } catch {
+
+    }
+
 }
--- src/routers/usersRouters.js
+++ src/routers/usersRouters.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { postSignup, postSignin } from "../controller/userController.js";
+import { postSignup, postSignin, showUserData } from "../controller/userController.js";
 import { validateUsersSchema } from "../middleware/postUserMiddleware.js";
 import  signupSchema from "../schemas/signupSchema.js"
 import signinSchema from "../schemas/signinSchema.js";
@@ -8,5 +8,6 @@ const router = Router()
 
 router.post("/signup", validateUsersSchema(signupSchema), postSignup)
 router.post("/signin", validateUsersSchema(signinSchema), postSignin)
+router.get("/users/me", showUserData)
 
 export default router
