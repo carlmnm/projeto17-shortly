abe4c4d0bda0ee30bc0906266bb15e2682beac07
carlmnm <carlossmiguell@gmail.com>
2023-03-03 03:09:17 -0300

    feat: POST /signin

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -30,9 +30,10 @@ export async function postSignin (req, res) {
 
     try {
         const token = uuid()
+        const myToken = {token: token}
         await db.query(`INSERT INTO tokens (user_id, token)
         VALUES ($1, $2);`, [user.rows[0].id, token])
-        res.send({token: token}).status(200)
+        res.send(myToken).status(200)
     } catch (error) {
         res.send(error)
     }
